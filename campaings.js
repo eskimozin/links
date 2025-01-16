bbf68145f78aa5373bf71e228d5013849e32cd77
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-01-16 19:05:34 -0300

    Updates

---
+++ campaings.js
@@ -0,0 +1,22 @@
+const campaings = [
+  {
+    "title": "Baixe e Jogue",
+    "subtitle": "Call Of Dragons",
+    "link": "https://vda.farlightgames.com/tracking?dap_code=11fe2de2d47730456aa723ed4b270a20&clickid=eskimozin",
+    "legend": "Os 21 primeiros que chegarem ao nível 7 ganharão VIP na live para 2025.",
+    "linkName": "Baixe clicando aqui",
+    "datetimeInit": "2024-12-06T00:00",
+    "datetimeFinish": "2025-12-13T23:59"
+  },
+  {
+    "title": "Content Insano",
+    "subtitle": "Live hoje jogando GeoGuessr",
+    "link": "https://twitch.tv/eskimozin",
+    "legend": "Acesse agora",
+    "linkName": "Twitch",
+    "datetimeInit": "2024-12-18T00:00",
+    "datetimeFinish": "2024-12-18T23:59"
+  }
+];
+
+export {campaings};
