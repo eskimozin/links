21c2aa9efae80082ea368bca28c9e95403cda1be
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-01-16 19:06:28 -0300

    fix: corrigindo data de campanha

--- public/campaings.js
+++ public/campaings.js
@@ -6,7 +6,7 @@ const campaings = [
     "legend": "Os 21 primeiros que chegarem ao nível 7 ganharão VIP na live para 2025.",
     "linkName": "Baixe clicando aqui",
     "datetimeInit": "2024-12-06T00:00",
-    "datetimeFinish": "2025-12-13T23:59"
+    "datetimeFinish": "2024-12-13T23:59"
   },
   {
     "title": "Content Insano",
