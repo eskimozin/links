3729415728b4977deaca72044d69c9a5beac5c71
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-13 20:57:14 -0300

    feat: links corrigidos, linkName da campanha de enxadahost tambem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
 
-import { Header } from './components/Header'
-import { Main } from './components/Main'
-import { Footer } from './components/Footer'
+import {Header} from "./components/Header";
+import {Main} from "./components/Main";
+import {Footer} from "./components/Footer";
 
 function App() {
   useEffect(() => {
