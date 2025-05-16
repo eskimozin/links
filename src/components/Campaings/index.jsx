6943d57a53ba446faa3a67b4cee801f1ea71e746
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-15 23:45:51 -0300

    feat: controle de exibicao das campanhas

--- src/components/Campaings/index.jsx
+++ src/components/Campaings/index.jsx
@@ -1,5 +1,6 @@
 import {Banner} from "../Banner/index.jsx";
 import {campaings} from "../../../public/campaings.js";
+import {useEffect, useState} from "react";
 
 const convertDatetime = (datetime) => {
   if (!datetime) return null
@@ -28,31 +29,60 @@ const sortedCampaings = (campaings) => {
 
 const Campaings = () => {
   const validCampaings = verifyIsValidCampaings(campaings)
+  const [show, setShow] = useState(true)
+  const [visible, setVisible] = useState(true)
+  
+  useEffect(() => {
+    if (show) setVisible(true);
+  }, [show]);
   
   if (validCampaings && validCampaings.length > 0) {
     return (
       <div className={"mt-2 mb-4 d-flex flex-column gap-3"}>
         <div className={"d-flex align-items-center justify-content-between gap-2 flex-wrap animate-from-bottom"}>
           <h2 className={"fs-5 text-white-50 fw-bold text-lowercase mb-0"} style={{fontFamily: "'Inter Tight', 'Inter', sans-serif"}}>campanhas</h2>
-          <button type={"button"} onClick={() => {alert("Sob implementação!")}} className={"btn btn-sm border-0 p-0 pe-1"}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF50" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
-              <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
-              <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
-            </svg>
+          <button type={"button"}
+                  onClick={() => setShow(!show)}
+                  className={"btn btn-sm border-0 p-0 pe-1"}
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  data-bs-custom-class="custom-tooltip"
+                  title={"Exibir ou ocultar"}
+          >
+            {
+              visible ? (
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF50" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                  <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+                  <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFF" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                </svg>
+              )
+            }
           </button>
         </div>
         {
-          sortedCampaings(validCampaings).map((c, i) => (
-            <div key={i}>
-              <Banner
-                title={c.title}
-                subtitle={c.subtitle}
-                link={c.link}
-                legend={c.legend}
-                linkName={c.linkName}
-              />
-            </div>
-          ))
+          visible ?
+            <div className={"d-flex flex-column gap-3 " + (show ? "animate-from-bottom" : "animate-hide-to-bottom") + " " + (visible ? "d-flex" : "d-none")} onAnimationEnd={() => {
+              if (!show) setVisible(false);
+            }}>
+              {
+                sortedCampaings(validCampaings).map((c, i) => (
+                  <div key={i}>
+                    <Banner
+                      title={c.title}
+                      subtitle={c.subtitle}
+                      link={c.link}
+                      legend={c.legend}
+                      linkName={c.linkName}
+                    />
+                  </div>
+                ))
+              }
+            </div> : ""
         }
       </div>
     )
