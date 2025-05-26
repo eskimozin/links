bc4dd7c795cbe09ac0556ec49c1ec959ceed0336
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-26 12:11:06 -0300

    feat: campanha do Norton VPN adicionada

--- public/campaings.js
+++ public/campaings.js
@@ -1,2 +1,72 @@
-const campaings = [{"title":"Batman: Arkham Asylum","subtitle":"a nova série do Noite dos Jogos Tristes","link":"https://www.youtube.com/@NoitedosJogosTristes","legend":"","linkName":"Assista agora, agorinha!","datetimeInit":"2025-05-21T00:00","datetimeFinish":"2025-05-21T23:59"},{"title":"Lives nas segundas, quartas, sextas e sábados","subtitle":"por volta das 20h, ou mais cedo ou mais tarde rs","link":"https://twitch.tv/eskimozin","legend":"Link ->","linkName":"twitch.tv/eskimozin","datetimeInit":"2025-05-12T00:00","datetimeFinish":"2025-05-22T23:59","recuperate":"Campanha recuperada - N.º 1","class":"d-none"},{"title":"Entre agora no nosso subreddit!","subtitle":"todas as quartas-feiras o eskimo reage aos posts de lá","link":"https://www.reddit.com/r/eskimozin/","legend":"Clica aí ->","linkName":"r/eskimozin","datetimeInit":"2025-04-22T00:00","datetimeFinish":"2025-06-23T23:59","recuperate":"Campanha recuperada - N.º 2","class":"d-none"},{"title":"Entre no nosso servidor","subtitle":"do Discord e mande vídeos legais","link":"https://discord.com/invite/fSzDQkBxcf","legend":"Pra entrar","linkName":"é só clicar aqui","datetimeInit":"2025-03-01T00:00","datetimeFinish":"2025-04-10T23:59","recuperate":"Campanha recuperada - N.º 3","class":"d-none"},{"title":"Murderhouse","subtitle":"a nova série do Noite dos Jogos Tristes","link":"https://www.youtube.com/@NoitedosJogosTristes","legend":"Clique ","linkName":"aqui e assista agora, agorinha!","datetimeInit":"2025-04-02T00:00","datetimeFinish":"2025-04-10T23:59","recuperate":"Campanha recuperada - N.º 4","class":"d-none"},{"title":"Use o Cupom \"INCIDENTE\" ","subtitle":"para obter 10% de desconto ao abrir seu servidor de Minecraft com a EnxadaHost!","link":"https://enxadahost.com/","legend":"Link ->","linkName":"enxadahost.com","datetimeInit":"2025-03-15T00:00","datetimeFinish":"2025-04-15T23:59","recuperate":"Campanha recuperada - N.º 5","class":"d-none"},{"title":"Canal de jogos com o JOHAN","subtitle":"Vídeos todos os dias, a partir de 27/01","link":"https://www.youtube.com/@NoitedosJogosTristes?sub_confirmation=1","legend":"Acesse agora","linkName":"e inscreva-se no canal!","datetimeInit":"2025-01-24T00:00","datetimeFinish":"2025-02-28T23:59","recuperate":"Campanha recuperada - N.º 6","class":"d-none"},{"title":"Baixe e Jogue","subtitle":"Call Of Dragons","link":"https://vda.farlightgames.com/tracking?dap_code=11fe2de2d47730456aa723ed4b270a20&clickid=eskimozin","legend":"Os 21 primeiros que chegarem ao nível 7 ganharão VIP na live para 2025.","linkName":"Baixe clicando aqui","datetimeInit":"2024-12-06T00:00","datetimeFinish":"2024-12-13T23:59","recuperate":"Campanha recuperada - N.º 7","class":"d-none"},{"title":"Content Insano","subtitle":"Live hoje jogando GeoGuessr","link":"https://twitch.tv/eskimozin","legend":"Acesse agora","linkName":"Twitch","datetimeInit":"2024-12-18T00:00","datetimeFinish":"2024-12-18T23:59","recuperate":"Campanha recuperada - N.º 8","class":"d-none"},{"title":"Content Insano","subtitle":"Live hoje jogando GeoGuessr","link":"https://twitch.tv/eskimozin","legend":"Acesse agora","linkName":"Twitch","datetimeInit":"2024-12-18T00:00","datetimeFinish":"2024-12-18T23:59","recuperate":"Campanha recuperada - N.º 9","class":"d-none"}];
-export { campaings };
+const campaings = [{"title": "Baixe e navegue com segurança", "subtitle": "com Norton VPN", "link": "https://odiencetrck.com/ipa/clk.php?lid=120", "legend": "Acesse agora", "linkName": "clicando aqui", "datetimeInit": "2025-05-26T00:00", "datetimeFinish": "2025-06-02T23:59"}, {
+  "title": "Batman: Arkham Asylum",
+  "subtitle": "a nova série do Noite dos Jogos Tristes",
+  "link": "https://www.youtube.com/@NoitedosJogosTristes",
+  "legend": "Assista ",
+  "linkName": "agora, agorinha!",
+  "datetimeInit": "2025-05-21T00:00",
+  "datetimeFinish": "2025-05-21T23:59",
+  "recuperate": "Campanha recuperada - N.º 1",
+  "class": "d-none"
+}, {
+  "title": "Lives nas segundas, quartas, sextas e sábados",
+  "subtitle": "por volta das 20h, ou mais cedo ou mais tarde rs",
+  "link": "https://twitch.tv/eskimozin",
+  "legend": "Link ->",
+  "linkName": "twitch.tv/eskimozin",
+  "datetimeInit": "2025-05-12T00:00",
+  "datetimeFinish": "2025-05-22T23:59",
+  "recuperate": "Campanha recuperada - N.º 2",
+  "class": "d-none"
+}, {"title": "Entre agora no nosso subreddit!", "subtitle": "todas as quartas-feiras o eskimo reage aos posts de lá", "link": "https://www.reddit.com/r/eskimozin/", "legend": "Clica aí ->", "linkName": "r/eskimozin", "datetimeInit": "2025-04-22T00:00", "datetimeFinish": "2025-06-23T23:59", "recuperate": "Campanha recuperada - N.º 3", "class": "d-none"}, {
+  "title": "Entre no nosso servidor",
+  "subtitle": "do Discord e mande vídeos legais",
+  "link": "https://discord.com/invite/fSzDQkBxcf",
+  "legend": "Pra entrar",
+  "linkName": "é só clicar aqui",
+  "datetimeInit": "2025-03-01T00:00",
+  "datetimeFinish": "2025-04-10T23:59",
+  "recuperate": "Campanha recuperada - N.º 4",
+  "class": "d-none"
+}, {"title": "Murderhouse", "subtitle": "a nova série do Noite dos Jogos Tristes", "link": "https://www.youtube.com/@NoitedosJogosTristes", "legend": "Clique ", "linkName": "aqui e assista agora, agorinha!", "datetimeInit": "2025-04-02T00:00", "datetimeFinish": "2025-04-10T23:59", "recuperate": "Campanha recuperada - N.º 5", "class": "d-none"}, {
+  "title": "Use o Cupom \"INCIDENTE\" ",
+  "subtitle": "para obter 10% de desconto ao abrir seu servidor de Minecraft com a EnxadaHost!",
+  "link": "https://enxadahost.com/",
+  "legend": "Link ->",
+  "linkName": "enxadahost.com",
+  "datetimeInit": "2025-03-15T00:00",
+  "datetimeFinish": "2025-04-15T23:59",
+  "recuperate": "Campanha recuperada - N.º 6",
+  "class": "d-none"
+}, {
+  "title": "Canal de jogos com o JOHAN",
+  "subtitle": "Vídeos todos os dias, a partir de 27/01",
+  "link": "https://www.youtube.com/@NoitedosJogosTristes?sub_confirmation=1",
+  "legend": "Acesse agora",
+  "linkName": "e inscreva-se no canal!",
+  "datetimeInit": "2025-01-24T00:00",
+  "datetimeFinish": "2025-02-28T23:59",
+  "recuperate": "Campanha recuperada - N.º 7",
+  "class": "d-none"
+}, {
+  "title": "Baixe e Jogue",
+  "subtitle": "Call Of Dragons",
+  "link": "https://vda.farlightgames.com/tracking?dap_code=11fe2de2d47730456aa723ed4b270a20&clickid=eskimozin",
+  "legend": "Os 21 primeiros que chegarem ao nível 7 ganharão VIP na live para 2025.",
+  "linkName": "Baixe clicando aqui",
+  "datetimeInit": "2024-12-06T00:00",
+  "datetimeFinish": "2024-12-13T23:59",
+  "recuperate": "Campanha recuperada - N.º 8",
+  "class": "d-none"
+}, {"title": "Content Insano", "subtitle": "Live hoje jogando GeoGuessr", "link": "https://twitch.tv/eskimozin", "legend": "Acesse agora", "linkName": "Twitch", "datetimeInit": "2024-12-18T00:00", "datetimeFinish": "2024-12-18T23:59", "recuperate": "Campanha recuperada - N.º 9", "class": "d-none"}, {
+  "title": "Content Insano",
+  "subtitle": "Live hoje jogando GeoGuessr",
+  "link": "https://twitch.tv/eskimozin",
+  "legend": "Acesse agora",
+  "linkName": "Twitch",
+  "datetimeInit": "2024-12-18T00:00",
+  "datetimeFinish": "2024-12-18T23:59",
+  "recuperate": "Campanha recuperada - N.º 10",
+  "class": "d-none"
+}];
+export {campaings};
