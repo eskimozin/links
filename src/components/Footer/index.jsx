26b1a25bdfc3e36b2ba09d30be2e9e9c0822f305
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-08-23 17:27:46 -0300

    feat: link do footer atualizado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,18 @@
 import './index.css'
 
 function Footer () {
-    const styles = {fontWeight: 300, fontFamily: '"Inter Tight", "Inter", sans-serif', letterSpacing: 0.20};
+    const styles = {fontFamily: '"Inter Tight", "Inter", sans-serif', letterSpacing: -0.20};
     
     return (
       <footer className={"footer"}>
-        <p className={"text text-muted text-always-balance"}>
-          <span className={"fw-light"} style={{...styles}}>Se algum link estiver desatualizado, avise os mods.</span><br/>
-          <a href={"https://github.com/gabriersdev"} className={"fw-light"} style={{...styles, color: "inherit"}}>Feito com 💖 pelo Gabriel.</a>
+        <p className={"text text-white text-always-balance"}>
+          <span className={"font-inter"} style={{...styles}}>Se algum link estiver desatualizado, avise os mods.</span><br/>
+          <a href={"https://github.com/gabriersdev"} className={"text-decoration-none d-flex align-items-center justify-content-center gap-1"} style={{...styles, color: "inherit"}}>
+            <span>Feito com 💖 pelo Gabriel</span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor">
+              <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/>
+            </svg>
+          </a>
         </p>
       </footer>
     );
