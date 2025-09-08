f04323962b4cd239ab4bd0beeacd9a06fa1001e9
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-09-08 00:15:06 -0300

    feat: botao para randomizacao do calculo

--- src/pages/Lives.jsx
+++ src/pages/Lives.jsx
@@ -20,7 +20,7 @@ function Lives() {
   const inputRef = useRef();
   
   useEffect(() => {
-    if (inputRef.current) inputRef.current?.focus();
+    focusInputRef();
   }, [inputRef]);
   
   // Lógica de Randomização
@@ -28,6 +28,10 @@ function Lives() {
   useEffect(() => {
     generateNewCaptcha();
   }, []);
+  
+  const focusInputRef = () => {
+    if (inputRef.current) inputRef.current?.focus();
+  }
   
   const generateNewCaptcha = () => {
     const newNum1 = Math.floor(Math.random() * 10) + 1; // Número aleatório de 1 a 10
@@ -99,8 +103,20 @@ function Lives() {
                 />
               </Modal.Body>
               
-              <Modal.Footer className={"bg-dark border-0 rounded-bottom-3"}>
-                <Button as={Link} to="/" variant="dark" tabIndex={-1}>Sair</Button>
+              <Modal.Footer className={"bg-dark border-0 rounded-bottom-3 d-flex gap-1 flex-wrap"}>
+                <Button variant="dark" tabIndex={-1} className={"py-0 px-2 m-0 bg-transparent border-0"} onClick={() => {
+                  generateNewCaptcha();
+                  focusInputRef();
+                }}>
+                  <div className={"d-flex align-items-center gap-1 flex-wrap"}>
+                    <span>Randomizar cálculo</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+                      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+                    </svg>
+                  </div>
+                </Button>
+                <Button as={Link} to="/" variant="dark" tabIndex={-1} className={"py-0 px-2 m-0 bg-transparent border-0"}>Sair</Button>
                 <Button variant="dark" className={"bg-theme border-0 text-dark fw-bold"} onClick={handleCaptchaSubmit}>
                   Confirmar!
                 </Button>
