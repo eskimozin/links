e1ffc7610e76cf7701fa0bdf390c20a19d4dbac5
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-09-23 20:22:49 -0300

    fix: correcao na obtencao do parametro "is_live"

--- src/components/LiveBadge/index.jsx
+++ src/components/LiveBadge/index.jsx
@@ -1,9 +1,10 @@
-import {useCallback, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {host, links} from "../../data/config.js";
 
 export default function LiveBadge() {
   const [liveInKick, setLiveInKick] = useState(false);
   const [liveInTwitch, setLiveInTwitch] = useState(false);
+  const [content, setContent] = useState(<></>);
   
   useEffect(() => {
     const endpoints = {
@@ -15,8 +16,8 @@ export default function LiveBadge() {
       fetch(`${host}/${endpoints[e]}`).then((res) => {
         return res.json()
       }).then(res => {
-        if (e === "kick" && res?.rows?.[0]) setLiveInKick(res?.rows?.[0]?.["isLive"])
-        else if (e === "twitch" && res?.rows?.[0]) setLiveInTwitch(res?.rows?.[0]?.["isLive"])
+        if (e === "kick" && res?.rows?.[0]) setLiveInKick(res?.["rows"]?.["0"]?.["0"]?.["is_live"])
+        else if (e === "twitch" && res?.rows?.[0]) setLiveInTwitch(res?.["rows"]?.["0"]?.["0"]?.["is_live"])
       }).catch(error => {
         console.error(`Error: ${error}`)
       })
@@ -24,38 +25,42 @@ export default function LiveBadge() {
     
   }, []);
   
-  const resContent = useCallback(() => {
-    if (liveInTwitch && liveInKick) {
-      return (
-        <a href={links["liveKick"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-5)"}}>
-          <div className={"live-indicator me-1"}>
-            <div className={"live-dot"}></div>
-          </div>
-          <span>Ao vivo na TWITCH e na KICK!</span>
-        </a>
-      )
-    } else if (liveInTwitch) {
-      return (
-        <a href={links["liveTwitch"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-6)"}}>
-          <div className={"live-indicator me-1"}>
-            <div className={"live-dot"}></div>
-          </div>
-          <span>Ao vivo na TWITCH!</span>
-        </a>
-      )
-    } else if (liveInKick) {
-      return (
-        <a href={links["liveKick"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-5)"}}>
-          <div className={"live-indicator me-1"}>
-            <div className={"live-dot"}></div>
-          </div>
-          <span>Ao vivo na KICK!</span>
-        </a>
-      )
-    }
+  useEffect(() => {
+    const resContent = () => {
+      if (liveInTwitch && liveInKick) {
+        return (
+          <a href={links["liveKick"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-5)"}}>
+            <div className={"live-indicator me-1"}>
+              <div className={"live-dot"}></div>
+            </div>
+            <span>Ao vivo na TWITCH e na KICK!</span>
+          </a>
+        )
+      } else if (liveInTwitch) {
+        return (
+          <a href={links["liveTwitch"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-6)"}}>
+            <div className={"live-indicator me-1"}>
+              <div className={"live-dot"}></div>
+            </div>
+            <span>Ao vivo na TWITCH!</span>
+          </a>
+        )
+      } else if (liveInKick) {
+        return (
+          <a href={links["liveKick"]} className={"animate-from-bottom rounded-pill text-decoration-none text-body mt-3 text-center px-3 py-1 d-inline-flex align-items-center mx-auto"} style={{background: "var(--gradient-5)"}}>
+            <div className={"live-indicator me-1"}>
+              <div className={"live-dot"}></div>
+            </div>
+            <span>Ao vivo na KICK!</span>
+          </a>
+        )
+      }
+    };
+    
+    setContent(resContent());
   }, [liveInKick, liveInTwitch]);
   
   return (
-    <article className={"animate-from-bottom"}>{resContent()}</article>
+    <article className={"animate-from-bottom"}>{content}</article>
   )
 }
